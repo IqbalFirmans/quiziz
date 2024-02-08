87c2ad0c8c3431e6f9a99611dfced6ff9a2970d4
Ghofur102 <muhamadabdulghofur84@gmail.com>
2024-02-08 11:00:17 +0700

    mengubah timezone, menyediakan data user dummy, dan memperbarui tampilan verify email.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,24 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(1)->create();
 
+        DB::table('users')->insert([
+            'name' => 'Ghofur',
+            'email' => 'Ghofur@gmail.com',
+            'password' => Hash::make('ghofur123'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Iqbal',
+            'email' => 'Iqbal@gmail.com',
+            'password' => Hash::make('iqbal123'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,6 +1,12 @@
 @extends('auth.layouts.main')
 @section('content')
     <div class="text-center">
-        <b>Lihat E-MAIL anda untuk verifikasi akun anda.</b>
+        @auth
+            @if (Auth::user()->email_verified_at == null)
+                <b>Lihat E-MAIL anda untuk verifikasi akun anda.</b>
+            @else
+                <b>Akun E-MAIL anda sudah terverifikasi.</b>
+            @endif
+        @endauth
     </div>
 @endsection
